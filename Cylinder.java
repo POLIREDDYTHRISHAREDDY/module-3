b659a6a8147f1fdec379246ff4eb95e320dd3b41
POLIREDDYTHRISHAREDDY <thrishareddy8@gmail.com>
2025-11-16 14:19:51 +0530

    Create Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,19 @@
+class Cylinder {
+    private double radius;
+    private double height;
+
+    // Constructor to initialize radius and height
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    // Getters
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
